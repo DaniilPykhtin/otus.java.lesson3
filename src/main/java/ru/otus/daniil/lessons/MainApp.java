dd954b28ff44e38650c8ae8a9840f29f782af5d9
daniil.pykhtin <daniil.pykhtin@tele2.ru>
2023-07-29 18:51:46 +0300

    Домашнее задание 7

--- src/main/java/ru/otus/daniil/lessons/MainApp.java
+++ src/main/java/ru/otus/daniil/lessons/MainApp.java
@@ -31,137 +31,115 @@ public class MainApp {
     }
 
     // Домашнее задание
-    public static int[] sumArrays(int[]... arrays) { // получается на вход идет матрица
+    public static int sumOfPositiveElements(int[][] matrix) {
+        int res = 0;
 
-        //дебаг
-        System.out.println("Входные массивы");
-        for (int i = 0; i < arrays.length; i++) {
-            System.out.println(Arrays.toString(arrays[i]));
-        }
-        //
-
-        int maxSize = arrays[0].length;
-
-        //нашли максимальный размер
-        for (int i = 1; i < arrays.length; i++) {
-            if (arrays[i].length > maxSize) {
-                maxSize = arrays[i].length;
-            }
-        }
-
-        int[] resArray = genArray(maxSize, true);
-
-        for (int i = 0; i < arrays.length; i++) {
-            for (int j = 0; j < arrays[i].length; j++) {
-                resArray[j] += arrays[i][j];
-            }
-        }
-        System.out.println("Выходной массив");
-        System.out.println(Arrays.toString(resArray));
-        return resArray;
-    }
-
-    public static boolean hasSpecialBound(int[] arr) {
-
-        System.out.println("Входной массив");
-        System.out.println(Arrays.toString(arr));
-
-        int fullSum = 0;
-        int leftSum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            fullSum += arr[i];
-        }
-
-        if (fullSum % 2 == 1) {
-            return false; //"Нет такой точки"
-        }
-
-        for (int i = 0; i < arr.length; i++) {
-            leftSum += arr[i];
-
-            if (leftSum == (fullSum / 2))
-                return true; //"Точка находится между элементами"
-        }
-        return false; //"Нет такой точки"
-    }
-
-    public static boolean isSorted(int[] arr, String sortType) {
-
-        System.out.println("Входной массив");
-        System.out.println(Arrays.toString(arr));
-        int prevValue = arr[0];
-        if (sortType.equals("desc"))  //идея посоветовала сравнивать так
-        {
-            for (int i = 1; i < arr.length; i++) {
-                if (prevValue < arr[i]) {
-
-                    //"Массив не отсортирован в нужном порядке"
-                    return false;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] > 0) {
+                    res += matrix[i][j];
                 }
-                prevValue = arr[i];
             }
-        } else if (sortType.equals("asc")) {
-            for (int i = 1; i < arr.length; i++) {
-                if (prevValue > arr[i]) {
-
-                    //"Массив не отсортирован в нужном порядке"
-                    return false;
-                }
-                prevValue = arr[i];
-            }
-        } else {
-            System.out.println("Введен неправильный порядок сортировки. Выберите asc или desc");
-            return false;
         }
-
-        return true; //"Массив отсортирован в нужном порядке"
-
+        return res;
     }
 
-    public static void reverseArray(int[] arr) {
-
-        System.out.println("Входной массив");
-        System.out.println(Arrays.toString(arr));
-        int buffer;
-
-        for (int i = 0; i < arr.length / 2; i++) {
-            buffer = arr[i];
-            arr[i] = arr[arr.length - 1 - i];
-            arr[arr.length - 1 - i] = buffer;
-
+    public static void printSquareBySize(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == 0 || j == 0 || i == size - 1 || j == size - 1) {
+                    System.out.print("  *");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.print("\n");
         }
-        System.out.println("Выходной массив");
-        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void setDiagonalValuesToZero(int[][] matrix, String method) {
+
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix.length != matrix[i].length) {  //если зубатую матрицу подсунут
+                System.out.println("Метод только для квадратных матриц");
+                return;
+            }
+        }
+        if (method.equalsIgnoreCase("anti")) { // я enum ом пока не умею пользоваться :(
+
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[matrix.length - 1 - i][i] = 0;
+            }
+        } else if (method.equalsIgnoreCase("both")) {
+            for (int i = 0; i < matrix.length; i++) {  //дважды центральный элемент занулит (если есть)
+                matrix[i][i] = 0;
+                matrix[matrix.length - 1 - i][i] = 0;
+            }
+        } else {  // по умолчанию - main
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][i] = 0;
+            }
+        }
+        // debug
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.println(Arrays.toString(matrix[i]));
+        }
+    }
+
+    public static int findMax(int[][] array) {
+        int max = array[0][0];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+
+                if (max < array[i][j]) {
+                    max = array[i][j];
+                }
+            }
+        }
+        return max;
+    }
+
+    public static int sumSecondRow(int[][] matrix) {
+        int res = 0;
+        if (matrix.length == 1) {
+            return -1;
+        }
+        for (int i = 0; i < matrix[1].length; i++) {
+            res += matrix[1][i];
+        }
+        return res;
     }
 
     public static void main(String[] args) {
+
+        //создадим случайную матрицу
+        int[][] matrix = new int[genIntWithinRange(1, 10)][genIntWithinRange(1, 10)];
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i] = genArray(matrix[i].length, false);
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.println(Arrays.toString(matrix[i]));
+        }
+        System.out.println();
+
         //1
-        System.out.println("Задача 1");
-        sumArrays(genArray(0, false), genArray(0, false), genArray(0, false));
+        System.out.println(sumOfPositiveElements(matrix));
         System.out.println();
-
         //2
-        //genArray(0, false)
-        System.out.println("Задача 2");
-        if (hasSpecialBound(genArray(0, false))) {  //new int[] {1,1,1,3,1,1,1,1,1,9}
-            System.out.println("Точка находится между элементами");
-        } else {
-            System.out.println("Точка НЕ находится между элементами");
-        }
-        System.out.println();
-
-        //3
-        System.out.println("Задача 3");
-        if (isSorted(genArray(0, false), "asc")) {
-            System.out.println("Массив отсортирован в нужном порядке");
-        } else {
-            System.out.println("Массив НЕ отсортирован в нужном порядке");
-        }
+        printSquareBySize(genIntWithinRange(1, 10));
         System.out.println();
         //4
-        System.out.println("Задача 4");
-        reverseArray(genArray(7, false));
+        System.out.println(findMax(matrix));
         System.out.println();
+        //5
+        System.out.println(sumSecondRow(matrix));
+        System.out.println();
+        //3
+        setDiagonalValuesToZero(matrix, "anti");
+        System.out.println();
+        //new int[][]{{1,1,1,1,1}, {1,1,1,1,1},{1,1,1,1,1},{1,1,1,1,1},{1,1,1,1,1}}
     }
+
 
 }
