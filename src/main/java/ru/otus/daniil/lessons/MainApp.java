494a7f5edde8db3508c05edce3c3ae36999a0ef6
DaniilPykhtin <139154988+DaniilPykhtin@users.noreply.github.com>
2023-10-02 09:27:54 +0300

    Update MainApp.java

--- src/main/java/ru/otus/daniil/lessons/MainApp.java
+++ src/main/java/ru/otus/daniil/lessons/MainApp.java
@@ -11,40 +11,23 @@ public class MainApp {
 
     public static void fourThreadPerfExample(double[] arr) throws InterruptedException {
 
-        Thread thread1 = new Thread(() -> {
-            for (int i = 0; i < arr.length / 4; i++) {
-                arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
-            }
-        });
+        Thread[] thread = new Thread[4];
 
-        Thread thread2 = new Thread(() -> {
-            for (int i = arr.length / 4; i < arr.length / 2; i++) {
-                arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
-            }
-        });
+        for (int i = 0; i < thread.length; i++) {
+            int finalI = i;
 
-        Thread thread3 = new Thread(() -> {
-            for (int i = arr.length / 2; i < arr.length * 3 / 4; i++) {
-                arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
-            }
-        });
+            thread[i] = new Thread(() -> {
+                for (int j = arr.length * finalI / 4; j < arr.length * (finalI + 1) / 4; j++) {
+                    arr[j] = 1.14 * Math.cos(j) * Math.sin(j * 0.2) * Math.cos(j / 1.2);
+                }
+            });
 
-        Thread thread4 = new Thread(() -> {
-            for (int i = arr.length * 3 / 4; i < arr.length; i++) {
-                arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
-            }
-        });
+            thread[i].start();
+        }
 
-        thread1.start();
-        thread2.start();
-        thread3.start();
-        thread4.start();
-        thread1.join();
-        thread2.join();
-        thread3.join();
-        thread4.join();
-
-
+        for (int i = 0; i < thread.length; i++) {
+            thread[i].join();
+        }
     }
 
 
