d7eb035b8c37f0c2c5efebad98f8ddbf69824ae1
DaniilPykhtin <139154988+DaniilPykhtin@users.noreply.github.com>
2023-10-04 17:06:38 +0300

    Create Orange.java

---
+++ src/main/java/ru/otus/daniil/lessons/Orange.java
@@ -0,0 +1,9 @@
+package ru.otus.daniil.pykhtin.lessons;
+
+public class Orange extends Fruit{
+
+    public static final float ORANGE_WEIGHT = 0.3f;
+    public Orange () {
+        super(ORANGE_WEIGHT);
+    }
+}
