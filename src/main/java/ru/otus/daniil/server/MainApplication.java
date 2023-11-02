74e8516e34aee77b1b76bd2d8874656e4b654700
daniil.pykhtin <daniil.pykhtin@tele2.ru>
2023-11-02 13:59:01 +0300

    homework31

---
+++ src/main/java/ru/otus/daniil/server/MainApplication.java
@@ -0,0 +1,53 @@
+package ru.otus.daniil.server;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MainApplication {
+    public static final int PORT = 8189;
+    public static int threadNum = 3;
+    public static int debugCnt = 0;
+    // + К домашнему задания:
+    // Добавить логирование!!!
+
+
+    public static void main(String[] args) {
+
+        Map<String, MyWebApplication> router = new HashMap<>();
+        router.put("/calculator", new CalculatorWebApplication());
+        router.put("/greetings", new GreetingsWebApplication());
+        router.put("/countdown", new TimerWebApplication());
+        Logger logger = LogManager.getLogger(MainApplication.class.getName());
+        ExecutorService eServise = Executors.newFixedThreadPool(threadNum);
+
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            logger.info("Сервер запущен, порт: " + PORT);
+
+            while (true) {
+                try  {
+                    Socket socket = serverSocket.accept();
+                    Handler handler = new Handler(socket, router, logger);
+                    eServise.execute(handler::run);
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
